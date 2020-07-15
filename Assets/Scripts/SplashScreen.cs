9f1d615a6b4a544a89e61eb8b0705abb96d5890b
xiangyumeng <xiangyumeng@tencent.com>
2020-07-15 16:57:45 +0800

    创建测试unity工程unity版本2018.3.14

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour
+{
+    public Text mText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("SplashScreen Start()");
+    }
+
+
+    public void SplashPlayEnd()
+    {
+        if(null != mText)
+        {
+            mText.text = "SplashPlayEnd";
+        }
+    }
+
+}
